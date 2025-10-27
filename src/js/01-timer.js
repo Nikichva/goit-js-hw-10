43d23c4ba4e68d11056a3522ad61c8e99225b577
Nikichva <niki.22.12.00@gmail.com>
2025-10-27 23:42:18 +0100

    initial for timer library

---
+++ src/js/01-timer.js
@@ -0,0 +1,18 @@
+'use strict';
+
+// commonjs
+const flatpickr = require('flatpickr');
+// Opisany w dokumentacji
+import flatpickr from 'flatpickr';
+// Dodatkowy import stylów
+import 'flatpickr/dist/flatpickr.min.css';
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
